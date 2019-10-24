9441579de28f62ff91063e95ea22f792eb3f9b7a
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-10-23 19:05:15 -0600

    First commit of a the NodeJS weather application

---
+++ app.js
@@ -0,0 +1,16 @@
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const location = process.argv[2]
+
+if (location) {geoCode(location, (error, { latitude, longitude, location }) => {
+    if (error) {
+        return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(location)
+        console.log(forecastData)
+      })
+    })} else {console.log('Please provide a location')}
